52a9b3895239a8970d21f55d4c6d00c12a4db35f
OfentseXI <dofentse69@gmail.com>
2024-03-15 09:28:59 +0200

    Update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    let resultMessage = document.getElementById('result'); // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-    if (/^pet_[a-zA-Z0-9]+$/.test(input)) {
-        result = "Valid Syntax";} 
+    if (/^pet_(20[0-1][0-9]|202[0-4])[A-Za-z]+$/.test(input)) {
+        result = "Valid Syntax 🟢";} 
           else {
-            result = "Invalid Syntax";}
+            result = "Invalid Syntax 🔴";}
         
           document.getElementById("result").innerText = result;
         }
